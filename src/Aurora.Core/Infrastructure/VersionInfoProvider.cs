eae8fea2e20a02db3545e4a19f478bdc5a9eeb48
Marek Dzikiewicz <marek.dzikiewicz@pm.me>
2021-05-08 16:56:09 +0200

    Core: VersionInfoProvider implementation

---
+++ src/Aurora.Core/Infrastructure/VersionInfoProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Aurora.Core.Infrastructure
+{
+    public class VersionInfoProvider : IStartupInfoProvider
+    {
+        public int Priority => 0;
+
+        public IEnumerable<string> Provide()
+        {
+            var name = Assembly.GetEntryAssembly()?.GetName();
+            var version = name?.Version;
+            var build = version?.Build > 0 ? version.Build : version?.Revision;
+
+            if (name != null && version != null)
+            {
+                yield return $"{name.Name} {version.Major}.{version.Minor} build {build}";
+            }
+        }
+    }
+}
